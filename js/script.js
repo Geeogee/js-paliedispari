6571c6ce72b5c2ee73dd3c6843a4189119602607
Gioele <gioele.armiento@gmail.com>
2021-03-12 15:27:18 +0100

    Exercise 1 finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-console.log("Hello world!");
+
+// Una funzione per capire se la parola è
+// palindroma
+// L'utente sceglie pari o dispari e un numero
+// da 1 a 5.
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+// Sommiamo i due numeri e dichiariamo chi ha
+// vinto.
+
+function palindromeWord(word) {
+
+    var reverseWord = "";
+    for (var i=word.length-1; i>=0; i--) {
+
+        reverseWord += word[i];
+    }
+
+    if(word == reverseWord) {
+
+        return true;
+    } else {
+
+        return false;
+    }
+}
+
+
+function exercise1() {
+    
+    var word = prompt("Please, insert a word");
+    var isPalindrome = palindromeWord(word);
+
+    if(isPalindrome) {
+
+        console.log("The word you choosed is a palindrome word!")
+    } else {
+
+        console.log("The word is not a palindrome word!")
+    }
+}
+
+exercise1();
